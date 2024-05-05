472cae325dcaebc3975d2a62d10a50747afef967
Kyle <140885143+kyle-gardner@users.noreply.github.com>
2024-05-05 06:08:09 -0400

    Basic CLI functional

---
+++ src/cli.rs
@@ -0,0 +1,17 @@
+pub fn help() {
+	println!("TODO!
+	Print all of the args");
+}
+
+pub fn user_mode(mut arguments: Vec<String>) {
+	arguments.remove(0);
+	
+
+}
+
+pub fn frontend_mode(mut arguments: Vec<String>) {
+	arguments.remove(0);
+
+	println!("front")
+
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
+use std::env::args;
+
+mod cli;
+
 fn main() {
-    println!("Hello, world!");
+	handle_args(args().collect());
+}
+
+fn handle_args(mut arguments: Vec<String>) {
+	// on windows, the first argument is the path to the executable
+	// on linux, i believe it doesnt do this, dont know about other OS
+	if std::env::consts::OS == "windows" {
+		arguments.remove(0);
+	}
+
+	if arguments.is_empty() {
+		// help 
+		cli::help();
+	} else {
+		if arguments.get(0).unwrap() == "-F" {
+			cli::frontend_mode(arguments);
+		} else {
+			cli::user_mode(arguments);	
+		}
+	}
 }
