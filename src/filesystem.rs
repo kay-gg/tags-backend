465f9b9eb09ab467b375110a9ca5533d1b9556bd
Kyle <140885143+kyle-gardner@users.noreply.github.com>
2024-05-06 02:09:37 -0400

    many things with Filesystem and Tag

--- src/filesystem.rs
+++ src/filesystem.rs
@@ -1,26 +1,40 @@
 use std::collections::HashMap;
 use serde::{Serialize, Deserialize};
 
-#[derive(Serialize, Deserialize)]
+#[derive(Serialize, Deserialize, PartialEq, Debug)]
 struct Filesystem {
 	tags: HashMap<String, Tag>,
 }
-
 impl Filesystem {
-	/// Creates a Filesystem with no Tags
-	/// 
-	/// Used for setup only
+	/// Creates a ```Filesystem``` with no ```Tag```s
 	fn new() -> Filesystem {
 		let h: HashMap<String, Tag> = HashMap::new();
-		let f = Filesystem {tags: h};
+		let f: Filesystem = Filesystem {tags: h};
 
 		return f;
 	}
+
+	/// Adds a ```Tag``` with ```name``` to the ```Filesystem```
+	fn create_tag(&mut self, name: &str) {
+		let t: Tag = Tag::new();
+		self.tags.insert(name.to_owned(), t);
+	}
 }
 
-#[derive(Serialize, Deserialize)]
+#[derive(Serialize, Deserialize, PartialEq, Debug)]
 struct Tag {
 	files: Vec<String>,
+}
+impl Tag {
+	/// Returns an empty Tag
+	fn new() -> Tag {
+		let t = Tag {files: Vec::new()};
+		return t;
+	}
+	
+	fn add_file(&mut self, path: &str) {
+		self.files.push(path.to_string());
+	}
 }
 
 #[cfg(test)]
@@ -36,9 +50,19 @@ mod filesystem_tests {
 	}
 
 	#[test]
-	fn adding_tags() {
-		let filesystem = Filesystem::new();
+	fn creating_tags() {
+		let mut filesystem = Filesystem::new();
 		filesystem.tags.insert("test".to_string(), Tag::new());
+
+		let mut test = Filesystem::new();
+		test.create_tag("test");
+
+		assert_eq!(filesystem, test);
+	}
+	#[test]
+	fn creating_two_same_tags() {
+		let mut f = Filesystem::new();
+		f.create_tag("test");
 	}
 }
 
@@ -48,7 +72,19 @@ mod tag_tests{
 
 	#[test]
 	fn empty_tag() {
+		let empty_tag = Tag {files: Vec::new()};
 
+		assert_eq!(empty_tag.files.is_empty(), Tag::new().files.is_empty());
 	}
 
+	#[test]
+	fn adding_files_to_tag() {
+		let mut tag = Tag::new();
+		tag.files.push("test".to_string());
+
+		let mut test = Tag::new();
+		test.add_file("test");
+		
+		assert_eq!(tag, test);
+	}
 }
