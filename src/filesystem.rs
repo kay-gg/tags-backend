714f74877c8b84194278604026921b26f15ca8d2
Kyle <140885143+kyle-gardner@users.noreply.github.com>
2024-05-06 05:04:31 -0400

    add_tags_to_file and create_tag works now yipeeee

--- src/filesystem.rs
+++ src/filesystem.rs
@@ -1,4 +1,4 @@
-use std::{collections::HashMap, ffi::OsString, path::{Path, PathBuf}};
+use std::{collections::HashMap, ffi::OsString, io::ErrorKind, path::{Path, PathBuf}};
 use serde::{Serialize, Deserialize};
 
 #[derive(Serialize, Deserialize, PartialEq, Debug)]
@@ -15,9 +15,32 @@ impl Filesystem {
 	}
 
 	/// Adds a ```Tag``` with ```name``` to the ```Filesystem```
-	fn create_tag(&mut self, name: &str) {
-		let t: Tag = Tag::new();
-		self.tags.insert(name.to_owned(), t);
+	fn create_tag(&mut self, name: &str) -> Result<(), ErrorKind> {
+		match self.tags.contains_key(name) {
+			false => {
+				let t: Tag = Tag::new();
+				self.tags.insert(name.to_owned(), t);
+				return Ok(());
+			}
+			true => Err(ErrorKind::AlreadyExists)
+		}
+	}
+	/// Adds file to each tag in ```tags```
+	/// 
+	/// name is misleading because we arent actually adding tags to the files itself,
+	/// but it sounds nicer this way. on the fence about changing
+	fn add_tags_to_file(&mut self, mut tags_vec: Vec<String>) {
+		// path is 0, for example
+		// {path} {tag1} ... {tagn}
+		let path = tags_vec.remove(0);
+
+		for t in tags_vec {
+			if let Some(tag) = self.tags.get_mut(&t) {
+				tag.add_file(&path);
+			} else {
+				println!("Tag: {} not found.", t);
+			}
+		}
 	}
 }
 
@@ -65,9 +88,49 @@ mod filesystem_tests {
 	#[test]
 	fn creating_two_same_tags() {
 		let mut f = Filesystem::new();
-		f.create_tag("test");
+		let _ = f.create_tag("test");
+		let test = f.create_tag("test");
 
-		
+		assert_eq!(Err(ErrorKind::AlreadyExists), test);
+	}
+// again, misleading
+	#[test]
+	fn adding_tag_to_f() {
+		let mut filesystem = Filesystem::new();
+		let _ = filesystem.create_tag("test1");
+		let _ = filesystem.create_tag("test2");
+		let mut tag = filesystem.tags.remove("test2").unwrap();
+		tag.add_file("./test");
+		filesystem.tags.insert("test2".into(), tag);
+
+		let mut test = Filesystem::new();
+		let _ = test.create_tag("test1");
+		let _ = test.create_tag("test2");
+		test.add_tags_to_file(vec!["./test".to_string(), "test2".to_string()]);
+
+		assert_eq!(filesystem, test);
+	}
+	#[test]
+	fn adding_tags_to_f() {
+		let mut filesystem = Filesystem::new();
+		let _ = filesystem.create_tag("test1");
+		let _ = filesystem.create_tag("test2");
+
+		let mut tag = filesystem.tags.remove("test1").unwrap();
+		tag.add_file("./test");
+		filesystem.tags.insert("test1".into(), tag);
+
+		let mut tag = filesystem.tags.remove("test2").unwrap();
+		tag.add_file("./test");
+		filesystem.tags.insert("test2".into(), tag);
+
+
+		let mut test = Filesystem::new();
+		let _ = test.create_tag("test1");
+		let _ = test.create_tag("test2");
+		test.add_tags_to_file(vec!["./test".to_string() ,"test1".to_string(), "test2".to_string()]);
+
+		assert_eq!(filesystem, test);
 	}
 }
 
@@ -83,7 +146,7 @@ mod tag_tests{
 	}
 
 	#[test]
-	fn adding_files_to_tag() {
+	fn adding_file_to_tag() {
 		let mut tag = Tag::new();
 		let abs = PathBuf::from("./test/").canonicalize().unwrap();
 		tag.files.insert(abs.file_name().unwrap().to_owned(), abs);
@@ -97,13 +160,11 @@ mod tag_tests{
 	#[test]
 	fn adding_file_twice() {
 		let mut tag = Tag::new();
-		let abs = PathBuf::from("./test/").canonicalize().unwrap();
-
-		tag.files.insert(abs.file_name().unwrap().to_owned(), abs);
+		tag.add_file("./test");
 
 		let mut test = Tag::new();
-		test.add_file("test");
-		test.add_file("test");
+		test.add_file("./test");
+		test.add_file("./test");
 
 		assert_eq!(tag, test);
 	}
