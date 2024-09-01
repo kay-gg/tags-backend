1d3a6664c55f06b36cb44e251759cdb79075669d
Kyle <140885143+kyle-gardner@users.noreply.github.com>
2024-09-01 13:40:00 -0400

    fix bug where -F output wasnt correct

--- src/frontend_mode.rs
+++ src/frontend_mode.rs
@@ -2,7 +2,8 @@ use crate::user_mode;
 
 pub fn give_full_filesystem() {
 	let fs = user_mode::get_filesystem().expect("Filesystem not found.");
-	println!("{:?}", fs);
+	let fs = serde_json::to_string(&fs).expect("Error serializing fs into string");
+	println!("{}", fs);
 }
 
 
